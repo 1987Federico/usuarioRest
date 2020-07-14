f5583c11141ecc6da263465e0df8f426ab96efde
federico <federicomatias.aguero1987@gmail.com>
2020-07-14 12:10:12 -0300

    first commit

--- server/config/config.js
+++ server/config/config.js
@@ -8,7 +8,7 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //Base de datos
-let urlDB = 'mongodb+srv://fede_celeste:Lanegracuca1905@cluster0-mwujc.mongodb.net/cafe';
+
 process.env.URLDB = urlDB;
 
 //vencimiento del token
@@ -16,4 +16,4 @@ process.env.CADUCIDAD_TOKEN =
 //Firma del token
 process.env.SEED = process.env.SEED || 'este-es-el-seed';
 // Google client ID
-process.env.CLIENT_ID = process.env.CLIENT_ID || '1036456029168-3tj1mu01hahepfpf4ud91jb56jo7e8v1.apps.googleusercontent.com';
+process.env.CLIENT_ID = process.env.CLIENT_ID 
